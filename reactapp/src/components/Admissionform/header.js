313d8f03b0ccbebab7b57c93775da1b1143070f5
murali0712 <uvmk2001@gmail.com>
2023-07-22 13:01:05 +0000

    admissionform

---
+++ reactapp/src/components/Admissionform/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function Header() {
+    return(
+        <nav className="bg-light navbar-dark navbar">
+            <div className="row col-12 d-flex justify-content-center text-black">
+                <center><h4>Application Form for Admission</h4></center>
+            </div>
+        </nav>
+    )
+}
+export default Header;
