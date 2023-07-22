184591694ceeeb20204ee25d05a5cd4f825b58eb
Yogapriyad <sit19it086@sairamtap.edu.in>
2023-07-22 13:25:29 +0000

    coursedetails

---
+++ reactapp/src/components/stud_dashboard/ProfilePage.js
@@ -0,0 +1,159 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import {privateAxios} from "./../../services/helper"
+
+const UserCredentials = () => {
+  const [userData, setUserData] = useState(
+    {
+      firstname: "",
+      lastname: "",
+      email: "",
+      password: "",
+      address: "",
+      phonenumber: ""
+    }
+  );
+  const [editingIndex, setEditingIndex] = useState(-1);
+  const [newFirstName, setNewFirstName] = useState('');
+  const [newLastName, setNewLastName] = useState('');
+  const [newEmail,setEmail]=useState('');
+  const [newPassword,setPassword]=useState('');
+  const [newAddress, setNewAddress] = useState('');
+  const [newPhoneNumber, setNewPhoneNumber] = useState('');
+
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    const dataString = localStorage.getItem('data');
+    const data = JSON.parse(dataString);
+    setUsers(data.user);
+    console.log(users);
+  }, []);
+
+  useEffect(() => {
+    if(users.id!=undefined){
+      fetchUserData();
+    }  
+  }, [users]);
+  
+  const fetchUserData = async () => {
+    try {
+      // change with id here pls
+      const response = await privateAxios.get(`/auth/students/${users.id}`);
+      setUserData(response.data);
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  };
+  const handleEdit = (index) => {
+    setEditingIndex(index);
+    setNewFirstName(userData.firstname);
+    setNewLastName(userData.lastname);
+    setEmail(userData.email);
+    setPassword(userData.password);
+    setNewAddress(userData.address);
+    setNewPhoneNumber(userData.phonenumber);
+  };
+  const handleSave = async () => {
+  try {
+    const updatedUser = {
+      id: userData.id,
+      firstName: newFirstName,
+      lastName: newLastName,
+      email: newEmail,
+      password: newPassword,
+      address: newAddress,
+      phoneNumber: newPhoneNumber,
+    };
+
+      await privateAxios.put(`/auth/students/${users.id}`, updatedUser);
+      setEditingIndex(-1);
+      fetchUserData();
+    } catch (error) {
+      console.error('Error updating user data:', error);
+    }
+  };
+
+  const handleCancel = () => {
+    setEditingIndex(-1);
+  };
+
+  return (
+    <div className='text-center' style={{"display" : "flex", "height": "90vh", "flexDirection": "column", "alignItems": "center", "justifyContent": "center"}}>
+       <p style={{ fontWeight: 'bolder' ,color:'#645CBB',fontSize:'30px' }}>USER PROFILE</p>
+        <div key={userData.id}>
+          {editingIndex === 1 ? (
+            <>
+	          <div>
+            <label style={{ fontWeight: 'bold' ,color:'#645CBB',fontSize:'20px' }}>FirstName:</label>
+              <input
+                type="text"
+                value={newFirstName}
+                onChange={(e) => setNewFirstName(e.target.value)}
+              />
+		        </div>
+	          <div>
+            <label style={{ fontWeight: 'bold' ,color:'#645CBB',fontSize:'20px' }}>LastName:</label>
+              <input
+                type="text"
+                value={newLastName}
+                onChange={(e) => setNewLastName(e.target.value)}
+              />
+		        </div>
+            <div>
+            <label style={{ fontWeight: 'bold' ,color:'#645CBB',fontSize:'20px' }}>Email:</label>
+              <input
+                type="text"
+                value={newEmail}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+		        </div>
+           {/* <div>
+            <label style={{ fontWeight: 'bold' ,color:'#645CBB',fontSize:'20px' }}>Password:</label>
+              <input
+                type="text"
+                value={newPassword}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+		        </div> */}
+           
+           <div>
+            <label style={{ fontWeight: 'bold' ,color:'#645CBB',fontSize:'20px' }}>Address:</label>
+              <input
+                type="text"
+                value={newAddress}
+                onChange={(e) => setNewAddress(e.target.value)}
+              />
+		      </div>
+           <div>
+            <label style={{ fontWeight: 'bold' ,color:'#645CBB',fontSize:'20px' }}>PhoneNumber:</label>
+              <input
+                type="text"
+                value={newPhoneNumber}
+                onChange={(e) => setNewPhoneNumber(e.target.value)}
+              />
+		      </div>
+              <button onClick={() => handleSave(1)} style={{ backgroundColor: '#645CBB',height: '40px',border: 'none',color:'white',borderRadius: '5px',marginRight:'20px',marginTop:'20px'}}>Save</button>
+              <button onClick={handleCancel} style={{ backgroundColor: '#645CBB',height: '40px',border: 'none',color:'white',borderRadius: '5px'}}>Cancel</button>
+            </>
+          ) : (
+            <>
+              <p className='course-name'>Name: {userData.firstname} {userData.lastname}</p>
+              <p className='course-name'>Email: {userData.email}</p>
+              {/* <p className='course-name'>Password: {userData.password}</p> */}
+              <p className='course-name'>Address: {userData.address}</p>
+              <p className='course-name'>PhoneNumber: {userData.phonenumber}</p>
+              <button className="form-btn" onClick={() => handleEdit(1)} >Edit</button>
+            </>
+          )}
+        </div>
+      
+    </div>
+  );
+};
+
+export default UserCredentials;
+         
+         
+         
+         
+         
---
+++ reactapp/src/components/stud_dashboard/Studentsidebar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './dash.css'
+
+
+const Studentsidebar = () => {
+  return (
+    <div className="col-md-5 col-lg-12 sidebar-offcanvas m-0 p-0" id="sidebar" role="navigation" style={{ backgroundColor: "#8294c439", minHeight: "100%" }}>
+      <ul className="nav flex-column sticky-top pt-5 p-3 ">
+        <li className="nav-item mb-2 "><a className="nav-link text-secondary" href="#"><h5 style={{ color: "#645CBB", fontFamily:"montserrat", fontWeight:"800"}}>DASHBOARD</h5></a></li>
+        {/*<div className="dropdown p-3">
+       <button className="btn btn-secondary dropdown-toggle"  style={{ backgroundColor: "#645CBB"}} href="#" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i className="fa fa-user" aria-hidden="true"></i> <span style={{ color: "white", fontFamily:"montserrat", fontWeight:"600" }}className="ml-3">Profile</span>
+          </button>
+          <ul className="dropdown-menu" >
+            <li><a className="dropdown-item" href="#"><i className="fa fa-regular fa-pen"></i> <span className="ml-3" style={{ color: "#645CBB", fontFamily:"montserrat", fontWeight:"400" }}>Edit profile</span></a></li>
+            <li><a className="dropdown-item" href="#"><i className="fa fa-solid fa-eye" aria-hidden="true"></i> <span className="ml-3" style={{ color: "#645CBB", fontFamily:"montserrat", fontWeight:"400" }}>View Profile</span></a></li>
+            <li><a className="dropdown-item" href="#"><i className="fa fa-regular fa-power-off" aria-hidden="true"></i> <span className="ml-3" style={{ color: "#645CBB", fontFamily:"montserrat", fontWeight:"400" }}>Sign Out</span></a></li>
+             
+          </ul>
+  </div>*/}
+        <li className="nav-item mb-2"><Link className="nav-link text-secondary" to="/main/profilePage"><i className="fa fa-user" aria-hidden="true"></i> <span className="ml-3" style={{ color: "#645CBB", fontFamily:"montserrat", fontWeight:"600" }}>Profile</span></Link></li>
+        <li className="nav-item mb-2"><Link className="nav-link text-secondary" to="/main/dashboard"><i className="fa fa-thin fa-city" aria-hidden="true"></i> <span className="ml-3" style={{ color: "#645CBB", fontFamily:"montserrat", fontWeight:"600" }}>Dashboard</span></Link></li>
+        <li className="nav-item mb-2"><Link className="nav-link text-secondary" to="/main/apply"><i className="fa fa-solid fa-file" aria-hidden="true"></i> <span className="ml-3" style={{ color: "#645CBB", fontFamily:"montserrat", fontWeight:"600" }}>Apply form</span></Link></li>
+        <li className="nav-item mb-2"><Link  className="nav-link text-secondary" to="/main/track"><i className=" fa fa-solid fa-eye" aria-hidden="true"></i> <span className="ml-3" style={{ color: "#645CBB", fontFamily:"montserrat", fontWeight:"600" }}>View status</span></Link></li>
+        <li className="nav-item mb-2"><Link className="nav-link text-secondary" to="/main/myenrollment"><i className="fas fa-file-export font-weight-bold"></i><span className="ml-3" style={{ color: "#645CBB", fontFamily:"montserrat", fontWeight:"600" }}>Enrollments</span></Link></li>
+        <li className="nav-item mb-2"><Link className="nav-link text-secondary" to="/main/courses"><i className="fa fa-thin fa-book"></i><span className="ml-3" style={{ color: "#645CBB", fontFamily:"montserrat", fontWeight:"600" }}>Courses</span></Link></li>
+        <li className="nav-item mb-2"><Link className="nav-link text-secondary" to="/main/notify"><i className="fa fa-regular fa-bell"></i><span className="ml-3" style={{ color: "#645CBB", fontFamily:"montserrat", fontWeight:"600" }}>Notifications</span></Link></li>
+       
+      </ul>
+    </div>
+  );
+}
+export default Studentsidebar
