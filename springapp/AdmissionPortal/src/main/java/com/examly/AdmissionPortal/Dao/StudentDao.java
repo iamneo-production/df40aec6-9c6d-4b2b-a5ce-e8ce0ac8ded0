11094a850997bed6e4f1767f0ae2766cd39a9a13
murali0712 <uvmk2001@gmail.com>
2023-07-23 13:50:55 +0000

    admission api

--- springapp/AdmissionPortal/src/main/java/com/examly/AdmissionPortal/Dao/StudentDao.java
+++
@@ -1,9 +0,0 @@
-package com.examly.AdmissionPortal.Dao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.examly.AdmissionPortal.Entities.Student;
-
-@Repository
-	public interface StudentDao extends JpaRepository<Student,Integer>{
-	}
